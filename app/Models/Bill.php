6f83b405beb6f1a7febbbb7dc936ad6b0f9081be
ewarandia <ewarandia@gmail.com>
2023-01-19 11:48:04 -0400

    correction relationship in models

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -8,4 +8,18 @@ use Illuminate\Database\Eloquent\Model;
 class Bill extends Model
 {
     use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'invoice_number',
+        'control_code',
+        'date',
+        'description',
+        'price',
+        'company_id',
+        'user_id'
+    ];
 }
