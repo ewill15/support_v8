fac0ecfe4a27aceda44c689eba8cefde8d5b25ed
Edwin <edwinarandia84@gmail.com>
2023-01-19 21:35:34 -0400

    update bill model

--- app/Models/Bill.php
+++ app/Models/Bill.php
@@ -8,4 +8,23 @@ use Illuminate\Database\Eloquent\Model;
 class Bill extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'invoice_number',
+        'control_code',
+        'date',
+        'description',
+        'price',
+        'company_id',
+        'user_id'
+    ];
+
+    public function company()
+    {
+        return $this->belongsTo(Company::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
