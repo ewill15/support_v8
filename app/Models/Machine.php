6c3cdda372deb9ae875937997fa95421e3f74b4c
Edwin <edwinarandia84@gmail.com>
2023-07-12 23:07:59 -0400

    add machine section

---
+++ app/Models/Machine.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Machine extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'motherboard',
+        'processor',
+        'IP',
+        'operative_system',
+        'mail_address',
+        'office_package',
+        'other'
+    ];
+    
+}
---
+++ database/migrations/2023_07_12_225801_create_machines_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMachinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('machines', function (Blueprint $table) {
+            $table->id();
+            $table->string('motherboard')->nullable();
+            $table->string('processor')->nullable();
+            $table->string('IP')->nullable();
+            $table->string('operative_system')->nullable();
+            $table->string('mail_address')->nullable();
+            $table->string('office_package')->nullable();
+            $table->text('other')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('machines');
+    }
+}
