7df9b74427715c834ab3bfd47d538aa9a314350f
ewarandia <ewarandia@gmail.com>
2025-02-22 19:28:29 -0400

    update login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,94 +1,69 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<title>{{env('APP_NAME')}} - LOGIN</title>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<!--===============================================================================================-->	
-	<link rel="icon" type="image/png" href="{{ asset('admin/images/icons/favicon.ico') }}"/>
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('admin/vendor/bootstrap/css/bootstrap.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('admin/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('admin/vendor/animate/animate.css') }}">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{ asset('admin/vendor/css-hamburgers/hamburgers.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('admin/vendor/animsition/css/animsition.min.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('admin/vendor/select2/select2.min.css') }}">
-<!--===============================================================================================-->	
-	<link rel="stylesheet" type="text/css" href="{{ asset('admin/vendor/daterangepicker/daterangepicker.css') }}">
-<!--===============================================================================================-->
-	<link rel="stylesheet" type="text/css" href="{{ asset('admin/css/util.css') }}">
-	<link rel="stylesheet" type="text/css" href="{{ asset('admin/css/main.css') }}">
-<!--===============================================================================================-->
+    <title>{{env('APP_NAME')}} - LOGIN</title>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet" />
+
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css"
+        href="{{ asset('admin/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="{{ asset('admin/css/login_v15/style.css') }}" />
 </head>
+
 <body>
-	
-	<div class="limiter">
-		<div class="container-login100">
-			<div class="wrap-login100">
-				@if (session('error'))
-					<div class="alert alert-danger">
-						{{ session('error') }}
-					</div>
-				@endif
-				<form id="frm-login" class="login100-form validate-form p-l-55 p-r-55 p-t-178" role="form" method="POST" action="{{ route('login') }}" autocomplete="off">
-                    {{ csrf_field() }}
-					<span class="login100-form-title">
-						{{env('APP_NAME')}}
-					</span>
+    <section class="ftco-section" style="background-color: #000bff47">
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-6 text-center mb-5">
+                    <h2 class="heading-section">{{env('APP_NAME')}}</h2>
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-md-7 col-lg-5">
+                    <div class="wrap">
+                        <div class="img" style="
+                                    background-image: url(img/login_test.jpg);
+                                "></div>
+                        <div class="login-wrap p-4 p-md-5">
+                            <form class="signin-form" method="POST" action="{{ route('login') }}" autocomplete="off"
+                                role="form">
+                                {{ csrf_field() }}
+                                <div class="form-group mt-3">
+                                    <input type="text" class="form-control" name="email" />
+                                    <label class="form-control-placeholder" for="username">{{ ucfirst(trans('common.email')) }}</label>
+                                </div>
+                                <div class="form-group">
+                                    <input id="password-field" type="password" class="form-control" name="password" />
+                                    <label class="form-control-placeholder mt-1" for="password">{{ ucfirst(trans('common.email')) }}</label>
+                                    <span toggle="#password-field"
+                                        class="fa fa-fw fa-eye field-icon toggle-password"></span>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="form-control btn btn-primary rounded submit px-3">
+                                        {{ ucfirst(trans('common.login')) }}
+                                    </button>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
 
-					<div class="wrap-input100 validate-input m-b-16" data-validate="Escriba su correo electronico">
-						<input class="input100" type="text" name="email" placeholder="Correo Electronico">
-						<span class="focus-input100"></span>
-					</div>
+    <!--===============================================================================================-->
+    <script src="{{ asset('admin/vendor/jquery/jquery-3.4.1.min.js') }}"></script>
 
-					<div class="wrap-input100 validate-input" data-validate = "Escriba su password">
-						<input class="input100" type="password" name="password" placeholder="Password">
-						<span class="focus-input100"></span>
-					</div>
-
-					<div class="text-right p-t-13 p-b-23">
-						{{-- <span class="txt1">
-							Forgot
-						</span>
-
-						<a href="#" class="txt2">
-							Username / Password?
-						</a> --}}
-					</div>
-
-					<div class="container-login100-form-btn my-5">
-						<button class="login100-form-btn" type="submit">
-							Sign in
-						</button>
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	
-	
-<!--===============================================================================================-->
-	<script src="{{ asset('admin/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('admin/vendor/animsition/js/animsition.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('admin/vendor/bootstrap/js/popper.js') }}"></script>
-	<script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('admin/vendor/select2/select2.min.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('admin/vendor/daterangepicker/moment.min.js') }}"></script>
-	<script src="{{ asset('admin/vendor/daterangepicker/daterangepicker.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('admin/vendor/countdowntime/countdowntime.js') }}"></script>
-<!--===============================================================================================-->
-	<script src="{{ asset('admin/js/main.js') }}"></script>
-	<script src="{{ asset('admin/js/custom.js') }}"></script>
-
+    <!--===============================================================================================-->
+    <script src="{{ asset('admin/vendor/bootstrap/js/popper43.js') }}"></script>
+    <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap43.min.js') }}"></script>
+    <!--===============================================================================================-->
+    <script src="{{ asset('admin/js/main43.js') }}"></script>
 </body>
+
 </html>
