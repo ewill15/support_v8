3cb8bb2fb2265fd3f5568768262197eed7ec4072
Edwin W. Arandia Zeballos <edwinarandia84@gmail.com>
2025-03-12 11:46:08 -0400

    fix style login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,10 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet" />
+    <!-- CUSTOM FONT-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="">
+    <link href="https://fonts.googleapis.com/css2?family=Kavivanar&amp;family=Pangolin&amp;display=swap" rel="stylesheet">
 
     <!--===============================================================================================-->
     <link rel="stylesheet" type="text/css"
