b569cdd4c0e47075140750045ca9498aa2afd67f
Edwin Willy Arandia Zeballos <TU_USUARIO@users.noreply.github.com>
2025-08-14 18:03:39 -0400

    update name login  page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-md-6 text-center mb-5">
-                    <h2 class="heading-section">{{strtoupper('support')}}</h2>
+                    <h2 class="heading-section">Maya's Boutique</h2>
                 </div>
             </div>
             <div class="row justify-content-center">
